c222f09e76aa500040f242d4342f4dae3545eef0
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-07 17:44:11 +0100

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+            saluto: "La mia prima app con Vite!"
+        }
+
+    }
+}
+</script>
+
+<template>
+    <h1 id="Title">{{ saluto }}</h1>
+</template>
+
+<style scoped>
+#title {
+    color: red;
+}
+</style>
